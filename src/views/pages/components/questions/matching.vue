c663ab3115ef907c47c716bc745474c9cc0c4d06
Ashraf Eldawody <ashraf6450@gmail.com>
2021-06-03 20:42:42 +0200

    matching added

--- src/views/pages/components/questions/matching.vue
+++ src/views/pages/components/questions/matching.vue
@@ -1,10 +1,31 @@
 <template>
-<p>matching</p>
+<div>
+  <editor @onChange="getQuestion" />
+  <v-divider></v-divider>
+
+  <v-card class="pa-4" tile>
+    <v-row v-for="i in 5" :key="i">
+      <v-col cols="12" md="5">
+      <v-text-field outlined></v-text-field>
+      </v-col>
+      <v-col cols="12" md="5">
+        <v-text-field outlined></v-text-field>
+      </v-col>
+      <v-col cols="12" md="2">
+        <v-text-field outlined type="number" min="1" value="1"></v-text-field>
+      </v-col>
+    </v-row>
+  </v-card>
+
+</div>
 </template>
 
 <script>
 export default {
-name: "matching"
+name: "matching",
+  data:()=>({
+    matches:[]
+  })
 }
 </script>
 
